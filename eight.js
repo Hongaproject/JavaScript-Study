6c0a8e3cb32c2af8a7232d1a81962f1872e8fb0a
Hongaproject <tkwlscjq99@gmail.com>
2023-03-24 15:24:30 +0900

    8day 파라미터

--- eight.js
+++ eight.js
@@ -1,23 +1,32 @@
 //파라미터
-function number (r) {
+function add (a,b) {
+    return a + b;
+}
+const sum = add(1,2);
+console.log(sum);
+
+
+const add1 = (a,b) => {
+    return a + b;
+}
+console.log(add1(1,2));
+
+
+function number1 (r) {
     return Math.PI*r*r;
 }
-const sum1 = number(1);
+const sum1 = number1(1);
 console.log(sum1);
 
 
-
-function number1 ( r = 1) {
+function number2 (r =1){
     return Math.PI*r*r;
 }
-const sum2 = number1();
+const sum2 = number2();
 console.log(sum2);
 
 
-
-const number2 = (r = 1) => {
+const number3 = (r = 1) => {
     return Math.PI*r*r;
 }
-
-const sum3 = number2();
-console.log(sum3);
+console.log(number3());
