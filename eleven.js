bdd1f623cc528621dcc5d3870ffc557422b8d8dc
Hongaproject <tkwlscjq99@gmail.com>
2023-03-30 22:15:48 +0900

    11day clock

--- eleven.js
+++ eleven.js
@@ -3,6 +3,7 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
+    //const hours = date.getHours();
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
