cb41bc72e007cc693279560d54d94cfbf9d711e9
Hongaproject <tkwlscjq99@gmail.com>
2023-11-01 22:30:00 +0900

    자바스크립트 변수

---
+++ JSbook/변수.js
@@ -0,0 +1,14 @@
+//변수 실습
+// 변수 a를 선언하고 변수 a의 값을 2로 만들어 보세요.
+var a = 2;
+
+var a;
+a = 2;
+
+// 변수 활용
+let name = prompt("이름을 입력해주세요");
+console.log(name, "님 안녕하세요.");
+
+//message를 console.log();를 이용해서 출력해 보세요.
+var message="날이 참 좋네요.";
+console.log(message);
---
+++ JSvelopert/변수.js
@@ -0,0 +1,28 @@
+// 변수 선언과 초기화
+// var, let, const 
+let c = 2; // 변수를 c로 선언하고 2로 초기화 시켜준다. 
+
+// var은 재선언와 재할당이 가능하다.
+var a = 1;
+var a = 2;
+
+// let은 재선언은 되지않고 재할당은 가능합니다.
+let ab = 3;
+ab = 5;
+// let ab = 7;
+
+// const는 재선언 재할당이 불가능 합니다.
+const abc = 1;
+// abc = 2;
+// const abc = 2;
+
+// 데이터타입 숫자(number), 문자열(string), 참/거짓(boolean), null과 undefined 
+let q = 1; 
+let text = '안녕하세요';
+let w = true;
+let e = false;
+
+let study = null; // 없다는 뜻 
+let yes; // undefined값이 설정이 되지 않았다.
+
+// null과 undefined는 고의 차이임. null은 내가 없다고 지정 한 것 undefined는 값을 지정 안한것.
