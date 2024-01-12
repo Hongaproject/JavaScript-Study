67cf3e1e934fa135be5126514eca7c964907b6f6
Hongaproject <tkwlscjq99@gmail.com>
2024-01-12 12:27:20 +0900

    프로그래머스 코딩테스트 입문 Day 8 문제

---
+++ 프로그래머스 알고리즘/8day.js
@@ -0,0 +1,40 @@
+// 프로그래머스 알고리즘 입문 Day 8          정답
+// 배열자르기
+
+
+// 외계행성의 나이
+
+
+// 진료순서 정하기
+
+
+// 순서쌍의 개수
+
+
+
+
+// 프로그래머스 알고리즘 입문 Day 8          문제
+// 배열자르기
+function solution(numbers, num1, num2) {
+    var answer = [];
+    return answer;
+    
+}
+
+// 외계행성의 나이
+function solution(age) {
+    var answer = '';
+    return answer;
+}
+
+// 진료순서 정하기
+function solution(emergency) {
+    var answer = [];
+    return answer;
+}
+
+// 순서쌍의 개수
+function solution(n) {
+    var answer = 0;
+    return answer;
+}
