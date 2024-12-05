2b593e211708225e11584d2f99cf4987c665bb92
Hongaproject <tkwlscjq99@gmail.com>
2024-12-05 20:53:50 +0900

    자료형

---
+++ 모던 자바스크립트/tutorial/1.js
@@ -0,0 +1,29 @@
+// 변수와 상수
+let msg;
+msg = "hello";
+alert(msg);
+
+let msg1 = "helloo";
+alert(msg1);
+
+let user = "John";
+let age = 25;
+let message = "Hello";
+
+let hello = "hello world";
+let msg2;
+
+msg2 = hello;
+
+alert(hello);
+alert(msg2);
+
+//과제
+let admin, name;
+name = "john";
+
+admin = name;
+alert(admin);
+
+// 변수명이 아주 길어지지 않는 이상, 설명을 좀 더 곁들어주는 것이 좋습니다.
+// 현재 접속 중인 사용자라는 정보를 담고 있는 이름으로 지어주는 것이 좋습니다.
