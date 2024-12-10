f99b821ab7c86809edde3f5c2ce61a74df0dec70
Hongaproject <tkwlscjq99@gmail.com>
2024-12-10 22:01:18 +0900

    논리연산자

--- 모던 자바스크립트/tutorial/1.js
+++ 모던 자바스크립트/tutorial/1.js
@@ -88,3 +88,22 @@ if (year < 2024) {
 } else {
   alert("정답입니다!");
 }
+
+let result = condition ? value1 : value2; // 삼항연산자
+
+let age = prompt('나이를 입력해주세요.', 18);
+let message = (age < 3) ? '아기야 안녕?' :
+  (age < 18) ? '안녕!' :
+  (age < 100) ? '환영합니다!' :
+  '나이가 아주 많으시거나, 나이가 아닌 값을 입력 하셨군요!';
+alert( message );
+
+if (age < 3) {
+    message = '아기야 안녕?';
+  } else if (age < 18) {
+    message = '안녕!';
+  } else if (age < 100) {
+    message = '환영합니다!';
+  } else {
+    message = '나이가 아주 많으시거나, 나이가 아닌 값을 입력 하셨군요!';
+  }
