3e415dd2383826dd971cdce71cfb6f3c3e99e577
Hongaproject <tkwlscjq99@gmail.com>
2024-12-30 22:58:10 +0900

    콜백함수

--- 모던 자바스크립트/tutorial/1.js
+++ 모던 자바스크립트/tutorial/1.js
@@ -241,3 +241,37 @@ let func = sayHi; // (2) 함수 복사
 
 func(); // Hello     // (3) 복사한 함수를 실행(정상적으로 실행됩니다)!
 sayHi(); // Hello    //     본래 함수도 정상적으로 실행됩니다.
+
+// 콜백함수
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+function showOk() {
+  alert("동의하셨습니다.");
+}
+
+function showCancel() {
+  alert("취소 버튼을 누르셨습니다.");
+}
+
+// 사용법: 함수 showOk와 showCancel가 ask 함수의 인수로 전달됨
+ask("동의하십니까?", showOk, showCancel);
+
+// 함수 ask의 인수, showOk와 showCancel은 콜백 함수 또는 콜백이라고 불립니다.
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "동의하십니까?",
+  function () {
+    alert("동의하셨습니다.");
+  },
+  function () {
+    alert("취소 버튼을 누르셨습니다.");
+  }
+);
